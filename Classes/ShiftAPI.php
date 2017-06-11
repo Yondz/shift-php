fd03f5373e6ac8154eb7d0f5594b0704949435a4
Grégory SURACI <gregory.suraci@smile.fr>
2017-06-11 15:36:30 +0200

    Added all api commands constants

--- Classes/ShiftAPI.php
+++ Classes/ShiftAPI.php
@@ -18,23 +18,103 @@ class ShiftAPI {
     /**
      * API Commands constants
      */
-    const START                     = "/api";
-    const ACCOUNTS                  = self::START . "/accounts";
-    const ACCOUNTS_GET_BALANCE      = self::ACCOUNTS . "/getBalance";
-    const ACCOUNTS_OPEN             = self::ACCOUNTS . "/open";
-    const ACCOUNTS_GET_PUBLICKEY    = self::ACCOUNTS . "/getPublicKey";
-    const ACCOUNTS_GEN_PUBLICKEY    = self::ACCOUNTS . "/generatePublicKey";
-    const ACCOUNTS_GET_DELEGATES    = self::ACCOUNTS . "/delegates";
-    const ACCOUNTS_PUT_DELEGATES    = self::ACCOUNTS_GET_DELEGATES;
-    const DELEGATES                 = self::START . "/delegates";
-    const DELEGATES_GET_VOTERS      = self::DELEGATES . "/voters";
+
+    // Root API
+    const START                             = "/api";
+
+    // Accounts
+    const ACCOUNTS                          = self::START . "/accounts";
+    const ACCOUNTS_GET                      = self::ACCOUNTS;
+    const ACCOUNTS_GET_BALANCE              = self::ACCOUNTS . "/getBalance";
+    const ACCOUNTS_OPEN                     = self::ACCOUNTS . "/open";
+    const ACCOUNTS_GET_PUBLICKEY            = self::ACCOUNTS . "/getPublicKey";
+    const ACCOUNTS_GEN_PUBLICKEY            = self::ACCOUNTS . "/generatePublicKey";
+    const ACCOUNTS_GET_DELEGATES            = self::ACCOUNTS . "/delegates";
+    const ACCOUNTS_PUT_DELEGATES            = self::ACCOUNTS . "/delegates";
+
+    // Loader
+    const LOADER                            = self::START . "/loader";
+    const LOADER_GET_STATUS                 = self::LOADER . "/status";
+    const LOADER_GET_SYNC                   = self::LOADER . "/status/sync";
+
+    // Transactions
+    const TRANSACTIONS                      = self::START . "/transactions";
+    const TRANSACTIONS_LIST                 = self::TRANSACTIONS;
+    const TRANSACTIONS_SEND                 = self::TRANSACTIONS;
+    const TRANSACTIONS_GET                  = self::TRANSACTIONS . "/get";
+    const TRANSACTIONS_UNCONFIRMED_LIST     = self::TRANSACTIONS . "/unconfirmed";
+    const TRANSACTIONS_UNCONFIRMED_GET      = self::TRANSACTIONS . "/unconfirmed/get";
+
+    // Peers
+    const PEERS                             = self::START . "/peers";
+    const PEERS_LIST                        = self::PEERS;
+    const PEERS_GET                         = self::PEERS . "/get";
+    const PEERS_GET_VERSION                 = self::PEERS . "/get/version";
+
+    // Blocks
+    const BLOCKS                            = self::START . "/blocks";
+    const BLOCKS_GET_ONE                    = self::BLOCKS . "/get";
+    const BLOCKS_GET_FILTER                 = self::BLOCKS;
+    const BLOCKS_GET_FEE                    = self::BLOCKS . "/getFee";
+    const BLOCKS_GET_FEES                   = self::BLOCKS . "/getFees";
+    const BLOCKS_GET_REWARD                 = self::BLOCKS . "/getReward";
+    const BLOCKS_GET_SUPPLY                 = self::BLOCKS . "/getSupply";
+    const BLOCKS_GET_HEIGHT                 = self::BLOCKS . "/getHeight";
+    const BLOCKS_GET_STATUS                 = self::BLOCKS . "/getStatus";
+    const BLOCKS_GET_NETHASH                = self::BLOCKS . "/getNethash";
+    const BLOCKS_GET_MILESTONE              = self::BLOCKS . "/getMilestone";
+
+    // Signatures
+    const SIGNATURES                        = self::START . "/signatures";
+    const SIGNATURES_GET                    = self::SIGNATURES . "/get";
+    const SIGNATURES_ADD_SECOND             = self::SIGNATURES;
+
+    // Delegates
+    const DELEGATES                         = self::START . "/delegates";
+    const DELEGATES_CREATE                  = self::DELEGATES;
+    const DELEGATES_LIST                    = self::DELEGATES;
+    const DELEGATES_GET                     = self::DELEGATES . "/get";
+    const DELEGATES_COUNT                   = self::DELEGATES . "/count";
+    const DELEGATES_GET_VOTES               = self::ACCOUNTS_GET_DELEGATES;
+    const DELEGATES_GET_VOTERS              = self::DELEGATES . "/voters";
+    const DELEGATES_ENABLE_FORGING          = self::DELEGATES . "/forging/enable";
+    const DELEGATES_DISABLE_FORGING         = self::DELEGATES . "/forging/disable";
+    const DELEGATES_GET_FORGED              = self::DELEGATES . "/forging/getForgedByAccount";
+
+    // Apps
+    const APPS                              = self::START . "/dapps";
+    const APPS_REGISTER                     = self::APPS;
+    const APPS_GET_REGISTERED               = self::APPS;
+    const APPS_GET                          = self::APPS;
+    const APPS_SEARCH                       = self::APPS . "/search";
+    const APPS_INSTALL                      = self::APPS . "/install";
+    const APPS_INSTALLED                    = self::APPS . "/installed";
+    const APPS_INSTALLED_IDS                = self::APPS . "/installedIds";
+    const APPS_UNINSTALL                    = self::APPS . "/uninstall";
+    const APPS_LAUNCH                       = self::APPS . "/launch";
+    const APPS_INSTALLING                   = self::APPS . "/installing";
+    const APPS_UNINSTALLING                 = self::APPS . "/uninstalling";
+    const APPS_LAUNCHED                     = self::APPS . "/launched";
+    const APPS_CATEGORIES                   = self::APPS . "/categories";
+    const APPS_STOP                         = self::APPS . "/stop";
+
+    // Multi-signature
+    const MULTI_SIGNATURE                   = self::START . "/multisignatures";
+    const MULTI_SIGNATURE_PENDING           = self::MULTI_SIGNATURE . "/pending";
+    const MULTI_SIGNATURE_CREATE_ACCOUNT    = self::MULTI_SIGNATURE;
+    const MULTI_SIGNATURE_SIGN              = self::MULTI_SIGNATURE . "/sign";
+    const MULTI_SIGNATURE_ACCOUNTS          = self::MULTI_SIGNATURE . "/accounts";
 
     /**
-     * ShiftAPI constructor. SSL enabled by default
+     * ShiftAPI constructor
      */
     public function __construct($host){
         $this->host =  $host;
     }
+
+    // =====================================================================================================
+    //  ACCOUNTS
+    // =====================================================================================================
 
     /**
      * Returns account information of an address.
@@ -159,6 +239,49 @@ class ShiftAPI {
     }
 
 
+    // =====================================================================================================
+    //  LOADER
+    // =====================================================================================================
+
+    /**
+     * Returns the status of the blockchain
+     * @return mixed
+     * @throws CommandException
+     */
+    public function getLoadingStatus(){
+        $command = new Command($this->host, self::LOADER_GET_STATUS,"GET");
+        return $command->execute();
+    }
+
+    /**
+     * Get the synchronization status of the client.
+     * @return mixed
+     * @throws CommandException
+     */
+    public function getSyncStatus(){
+        $command = new Command($this->host, self::LOADER_GET_SYNC,"GET");
+        return $command->execute();
+    }
+
+    // =====================================================================================================
+    //  TRANSACTIONS
+    // =====================================================================================================
+
+    // =====================================================================================================
+    //  PEERS
+    // =====================================================================================================
+
+    // =====================================================================================================
+    //  BLOCKS
+    // =====================================================================================================
+
+    // =====================================================================================================
+    //  SIGNATURES
+    // =====================================================================================================
+
+    // =====================================================================================================
+    //  DELEGATES
+    // =====================================================================================================
 
     /**
      * Get voters of delegate.
@@ -173,6 +296,12 @@ class ShiftAPI {
         return $command->getData("accounts");
     }
 
+    // =====================================================================================================
+    //  APPS
+    // =====================================================================================================
 
+    // =====================================================================================================
+    //  MULTI-SIGNATURE
+    // =====================================================================================================
 
 }
